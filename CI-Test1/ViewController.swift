568b458730a6704e2663356a187c7e6562d3774c
ldupisani <louisdupisani@yahoo.co.uk>
2018-07-16 11:07:34 +0200

    Initial Commit

---
+++ CI-Test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CI-Test1
+//
+//  Created by Louis du Pisani on 2018/07/16.
+//  Copyright © 2018 Louis du Pisani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
